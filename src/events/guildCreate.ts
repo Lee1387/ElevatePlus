7ba85e4688bc330ec81058f77987c343062e690b
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-09-02 03:03:48 +0100

    bug fixes, ground for level roles permissions

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -2,19 +2,17 @@ import { BaseChannel, ChannelType, TextChannel } from "discord.js";
 import { Event } from "../interfaces";
 import { createGuild } from "../modules/guild/";
 import { updatePresence } from "../modules/presence/";
-import { assignLevelRolesInGuild } from "../modules/roles";
-import { setDefaultChannelId } from "../modules/guild/";
+import { assignLevelRolesInGuild } from "../modules/roles/";
+import { setDefaultChannelId } from "../modules/guild";
 
 export const guildCreate: Event = {
     name: "guildCreate",
     run: async (client, guild) => {
-        await createGuild(guild);
-
-        // decided not to create users on guild join due to heavy join due to heavy load in case of bot being added to a large server 
-        // await createUsers(guild);
-
+        const sourceGuild = await createGuild(guild);
         await updatePresence(client);
-        await assignLevelRolesInGuild(client, guild);
+
+        if(sourceGuild.levelRoles)
+            await assignLevelRolesInGuild(client, guild);
 
         const owner = await client.users.fetch(guild.ownerId);
         const textChannels = guild.channels.cache.filter((channel: BaseChannel) => channel.type === ChannelType.GuildText);
